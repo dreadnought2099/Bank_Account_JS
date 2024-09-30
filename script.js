9e4c82372569f720a32d818231c2028de86e18d7
dreadnought2099 <raymart.magallanes@bisu.edu.ph>
2024-09-30 14:14:11 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let balance = 0;
+
+function checkBal() {
+  document.getElementById("message").innerText = "Your balance is " + balance;
+}
+
+function dep() {
+  let depositAmount = prompt("Enter amount to deposit");
+  if (depositAmount && !isNaN(depositAmount)) {
+    balance += parseFloat(depositAmount);
+    document.getElementById("balance").value = balance;
+    document.getElementById("message").innerText = "Deposit successful.";
+  } else {
+    document.getElementById("message").innerText = "Invalid deposit amount";
+  }
+}
+
+function withdraw() {
+  let withdrawAmount = prompt("Enter amount to withdraw:");
+  if (withdrawAmount && !isNaN(withdrawAmount)) {
+    if (balance >= withdrawAmount) {
+      balance -= parseFloat(withdrawAmount);
+      document.getElementById("balance").value = balance;
+      document.getElementById("message").innerText = "Withdrawal successful.";
+    } else {
+      document.getElementById("message").innerText = "Insufficient funds.";
+    }
+  } else {
+    document.getElementById("message").innerText = "Invalid withdrawal amount.";
+  }
+}
+
+function exitATM() {
+  balance = 0;
+  document.getElementById("balance").value = balance;
+  document.getElementById("message").innerText = "THANK YOU FOR USING ATM";
+}
